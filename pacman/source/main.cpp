9a7506187071177065932aa76055d747cdddada6
tom-goring <tgoring97@gmail.com>
2019-02-08 00:06:46 +0000

    Add code for working game

--- pacman/source/main.cpp
+++ pacman/source/main.cpp
@@ -1,36 +1,159 @@
 #include "MicroBit.h"
+#include <vector>
 
-struct Point {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
 
+void handle_player_movement();
+void handle_enemy_movement();
+void handle_screen_updates();
+void track_score();
+void initialise_game();
+void check_game_status();
+
+class Player {
+public:
     int16_t x;
     int16_t y;
+    int score;
+    void init() {
+        x = 2;
+        y = 2;
+        score = 0;
+    }
 };
 
-MicroBit        uBit;
-MicroBitImage   grid(5, 5);
-int             score;
-int             game_over;
-int             level;
-int             INVADER_SPEED;
-int             PLAYER_SPEED;
-int             BULLET_SPEED;
-Point           player;
-Point           bullet;
+class Enemy {
+public:
+    int16_t x;
+    int16_t y;
+    Enemy() {
+        x = rand() % 4;
+        y = rand() % 4;
+    }
+};
 
+
+MicroBit            uBit;
+std::vector<Enemy*>  enemies;
+Player              player;
+bool                game_over;
 
 
 int main() {
 
-    player.x = 2;
-    player.y = 4;
+    uBit.init();
 
-    while (true) {
+    initialise_game();
 
-        uBit.sleep(10);
-        uBit.display.image.clear();
+    create_fiber(handle_player_movement);
+    create_fiber(handle_enemy_movement);
+    create_fiber(track_score);
 
-        uBit.serial.send(uBit.accelerometer.getX());
+    while (!game_over) {
 
-        uBit.display.image.setPixelValue(player.x, player.y, 255);
+        handle_screen_updates();
+        check_game_status();
+    }
+
+    uBit.serial.send(game_over);
+
+    uBit.display.scroll("GAME OVER");
+    uBit.display.scroll("SCORE:");
+    uBit.display.print(player.score);
+}
+
+void handle_player_movement() {
+
+    while (!game_over) {
+
+        uBit.sleep(250);
+
+        if (uBit.accelerometer.getX() < -300 && player.x != 0) {
+
+            player.x--;
+        }
+        else if (uBit.accelerometer.getX() > 300 && player.x != 4) {
+
+            player.x++;
+        }
+        else if (uBit.accelerometer.getY() < -300 && player.y != 0) {
+
+            player.y--;
+        }
+        else if (uBit.accelerometer.getY() > 300 && player.y != 4) {
+
+            player.y++;
+        }
     }
 }
+
+void handle_enemy_movement() {
+
+    while (!game_over) {
+
+        uBit.sleep(500);
+
+        for (Enemy* enemy : enemies) {
+
+            if (player.x < enemy->x && enemy->x != 0) {
+
+                enemy->x--;
+            }
+            else if (player.x > enemy->x && enemy->x != 4) {
+
+                enemy->x++;
+            }
+            else if (player.y < enemy->y && enemy->y != 0) {
+
+                enemy->y--;
+            }
+            else if (player.y > enemy->y && enemy->y != 4) {
+
+                enemy->y++;
+            }
+        }
+    }
+}
+
+void handle_screen_updates() {
+
+    uBit.sleep(10);
+    uBit.display.clear();
+    uBit.display.image.setPixelValue(player.x, player.y, 255);
+    for (Enemy* enemy : enemies) {
+
+        uBit.display.image.setPixelValue(enemy->x, enemy->y, 255);
+    }
+}
+
+void track_score() {
+
+    while (!game_over) {
+
+        uBit.sleep(2500);
+        player.score++;
+    }
+}
+
+void initialise_game() {
+
+    player.init();
+    enemies.clear();
+    enemies.push_back(new Enemy);
+    game_over = false;
+    uBit.display.image.setPixelValue(player.x, player.y, 255);
+}
+
+void check_game_status() {
+
+    for (Enemy* enemy : enemies) {
+
+        if (player.x == enemy->y && player.y == enemy->y) {
+
+            game_over = true;
+        }
+    }
+}
+
+#pragma clang diagnostic pop
